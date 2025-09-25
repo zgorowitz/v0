fd80d4de1fc41567310b7f6ac2720d2554efac09
zgorowitz <zgorowitz@gmail.com>
2025-09-25 17:56:23 -0400

    m

--- app/(pages)/scan2/scan/page.tsx
+++ app/(pages)/scan2/scan/page.tsx
@@ -238,7 +238,7 @@ function ScanPage() {
       <div className="fixed inset-0 bg-gradient-to-b from-gray-50 to-white" />
 
       {/* Main Content - Mobile First */}
-      <main className="relative z-10 h-[calc(100vh-4rem)] flex flex-col">
+      <main className="relative z-10 h-[80vh] flex flex-col">
         {/* Top Section - Controls */}
         <div className="w-full px-4 pt-safe pb-4">
           <div className="max-w-lg mx-auto space-y-3">
